0ada7eebf81a45c047ce62c2d75d7e660b24cdaf
opwe37 <opwe37@naver.com>
2021-07-19 15:41:05 +0900

    7/19 Django class _ block AccountUpdateView's username field

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
@@ -41,10 +42,11 @@ class AccountDetailView(DetailView):
 
 class AccountUpdateView(UpdateView):
     model = User
-    form_class = UserCreationForm
+    form_class = AccountUpdateForm
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
 
 class AccountDeleteView(DeleteView):
     model = User
