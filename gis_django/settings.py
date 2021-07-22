c10880ee03250d3a26b277691eb1b12990aae67d
opwe37 <opwe37@naver.com>
2021-07-22 16:02:19 +0900

    7/22 Django class _ Create Profileapp

--- gis_django/settings.py
+++ gis_django/settings.py
@@ -52,11 +52,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'bootstrap4',
-
     'accountapp',
-    'profileapp'
+    'profileapp',
 ]
 
 MIDDLEWARE = [
--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')   # User 모델과 1:1 관계를 만겠다. 외래키 설정
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=30, unique=True)
+    message = models.CharField(max_length=255, null=True)
