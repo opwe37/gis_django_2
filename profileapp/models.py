c959475c544d487d5a8fc49f1a0017482bee8b58
opwe37 <opwe37@naver.com>
2021-07-26 16:09:40 +0900

    7/26 Django class _ Profile Create & Detail UI modified

--- profileapp/models.py
+++ profileapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,
-                                related_name='profile')   # User 모델과 1:1 관계를 만겠다. 외래키 설정
+    user = models.OneToOneField(User, on_delete=models.CASCADE,     # User 모델과 1:1 관계를 만겠다. 외래키 설정
+                                related_name='profile')             # related_name: User가 이 Profile 모델을 어떤 이름으로 바라볼 것인가
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=30, unique=True)
     message = models.CharField(max_length=255, null=True)
