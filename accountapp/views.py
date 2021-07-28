13b774d3a9568dc0de33dc66d88e25563d2832c2
opwe37 <opwe37@gmail.com>
2021-07-28 11:05:41 +0900

    07.28 - add update profile2

--- accountapp/views.py
+++ accountapp/views.py
@@ -46,8 +46,10 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 class AccountDetailView(DetailView):
