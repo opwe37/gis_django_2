fb27261b573eac75eb2704ebde68a32c5f5d3b43
opwe37 <opwe37@gmail.com>
2021-08-02 15:26:31 +0900

    08.02 - [Create] Article Model, Form

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,11 +1,4 @@
-from django.forms import ModelForm
 from django.shortcuts import render
 
 # Create your views here.
-from articleapp.models import Article
 
-
-class ArticleCreationForm(ModelForm):
-    class Meta:
-        model = Article
-        fields = ['title', 'image', 'content']
