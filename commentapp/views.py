dccbea87444adedbbb0c1f3b88c3977eed7d793f
opwe37 <opwe37@gmail.com>
2021-08-09 15:18:21 +0900

    08.09 - [Modify] Comment View

--- commentapp/views.py
+++ commentapp/views.py
@@ -16,3 +16,7 @@ class CommentCreateView(CreateView):
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
 
+    def form_valid(self, form):
+        form.istance.writer = self.request.user
+        form.instace.article_id = self.request.POST.get('article_pk')
+        return super().form_valid(form)
