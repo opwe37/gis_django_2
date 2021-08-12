af69309bc63dbdf2554f84fdb34d6de3c89edf9a
opwe37 <opwe37@gmail.com>
2021-08-12 15:17:43 +0900

    08.12 - [Create] Init ProjectApp

--- gis_django/urls.py
+++ gis_django/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('profile/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
+    path('project/', include('projectapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
